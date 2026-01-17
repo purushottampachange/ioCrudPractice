15596ce63df7304207ed93939bea0641ec4df8c9
purushottam pachange <purushottampachnage@gmail.com>
2026-01-17 15:41:30 +0530

    change some data

--- src/app/shared/consts/movie.ts
+++ src/app/shared/consts/movie.ts
@@ -1,46 +1,34 @@
-import { Imovie } from "../model/movie";
+import { Imovie } from '../model/movie';
 
-export const movies : Array<Imovie> = [
-  {
-    id: 1,
-    title: "Inception",
-    description: "A thief who steals corporate secrets through dream-sharing technology.",
-    rating: 8.8,
-    image: "https://image.tmdb.org/t/p/w500/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg"
-  },
-  {
-    id: 2,
-    title: "Interstellar",
-    description: "A team of explorers travel through a wormhole in space to ensure humanity's survival.",
-    rating: 8.6,
-    image: "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg"
-  },
+export const movies: Array<Imovie> = [
   {
     id: 3,
-    title: "The Dark Knight",
-    description: "Batman faces the Joker, a criminal mastermind who plunges Gotham into chaos.",
+    title: 'The Dark Knight',
+    description:
+      'Batman faces the Joker, a criminal mastermind who plunges Gotham into chaos.',
     rating: 9.0,
-    image: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg"
+    image: 'https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg',
   },
   {
     id: 4,
-    title: "Avengers: Endgame",
+    title: 'Avengers: Endgame',
     description: "The Avengers assemble once more to reverse Thanos' actions.",
     rating: 8.4,
-    image: "https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg"
+    image: 'https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg',
   },
   {
     id: 5,
-    title: "Joker",
-    description: "A failed comedian descends into madness and becomes Gotham's most feared villain.",
+    title: 'Joker',
+    description:
+      "A failed comedian descends into madness and becomes Gotham's most feared villain.",
     rating: 8.5,
-    image: "https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg"
+    image: 'https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg',
   },
   {
     id: 6,
-    title: "Titanic",
-    description: "A love story unfolds aboard the ill-fated RMS Titanic.",
+    title: 'Titanic',
+    description: 'A love story unfolds aboard the ill-fated RMS Titanic.',
     rating: 7.9,
-    image: "https://image.tmdb.org/t/p/w500/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg"
-  }
+    image: 'https://image.tmdb.org/t/p/w500/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg',
+  },
 ];
